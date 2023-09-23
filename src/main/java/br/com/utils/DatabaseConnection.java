04574c9c62a42247f6b5a64fe6546d1597c30eef
exartt <lmoraes1644cadastros@gmail.com>
2023-09-23 20:00:15 -0300

    up3

--- src/main/java/br/com/utils/DatabaseConnection.java
+++ src/main/java/br/com/utils/DatabaseConnection.java
@@ -2,6 +2,8 @@ package br.com.utils;
 
 import org.yaml.snakeyaml.Yaml;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -9,14 +11,10 @@ import java.sql.SQLException;
 import java.util.Map;
 
 public class DatabaseConnection {
-    private static final String CONFIG_FILE = "config.yml";
-
     public static Connection getConnection() throws SQLException {
-        try (InputStream input = DatabaseConnection.class.getClassLoader().getResourceAsStream(CONFIG_FILE)) {
-            if (input == null) {
-                throw new RuntimeException("Arquivo de configuração " + CONFIG_FILE + " não encontrado.");
-            }
+        String configFilePath = "src/main/resources/config.yml";
 
+        try (FileInputStream input = new FileInputStream(configFilePath)) {
             Yaml yaml = new Yaml();
             Map<String, Object> config = yaml.load(input);
             Map<String, String> databaseConfig = (Map<String, String>) config.get("database");
@@ -26,7 +24,7 @@ public class DatabaseConnection {
             String password = databaseConfig.get("password");
 
             return DriverManager.getConnection(url, user, password);
-        } catch (Exception e) {
+        } catch (IOException e) {
             throw new SQLException("Erro ao ler configurações do banco de dados.", e);
         }
     }
