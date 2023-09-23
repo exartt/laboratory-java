fdc7f3bf404b8e941331bc87b3cd100e71864fb7
exartt <lmoraes1644cadastros@gmail.com>
2023-09-23 19:45:03 -0300

    Single thread version ready to go.

--- src/main/java/br/com/utils/DatabaseConnection.java
+++ src/main/java/br/com/utils/DatabaseConnection.java
@@ -1,15 +1,33 @@
 package br.com.utils;
 
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Map;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:postgresql://babar.db.elephantsql.com:5432/ysetqhyu";
-    private static final String USER = "ysetqhyu";
-    private static final String PASSWORD = "";
+    private static final String CONFIG_FILE = "config.yml";
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        try (InputStream input = DatabaseConnection.class.getClassLoader().getResourceAsStream(CONFIG_FILE)) {
+            if (input == null) {
+                throw new RuntimeException("Arquivo de configuração " + CONFIG_FILE + " não encontrado.");
+            }
+
+            Yaml yaml = new Yaml();
+            Map<String, Object> config = yaml.load(input);
+            Map<String, String> databaseConfig = (Map<String, String>) config.get("database");
+
+            String url = databaseConfig.get("url");
+            String user = databaseConfig.get("user");
+            String password = databaseConfig.get("password");
+
+            return DriverManager.getConnection(url, user, password);
+        } catch (Exception e) {
+            throw new SQLException("Erro ao ler configurações do banco de dados.", e);
+        }
     }
 }
